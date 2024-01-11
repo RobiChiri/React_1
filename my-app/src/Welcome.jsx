d779c3b37046e385bfd843003c5440d005dd8bc9
Robichiri <robertochiriac8@gmail.com>
2024-01-11 15:13:28 +0100

    Controlled Forms - 1

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -5,7 +5,7 @@ export function Welcome({ name, age }) {
   return (
     <div>
       <p>Welcome, {name}!</p>
-      <p>{age > 18 ? <Age age={age} /> : <p>You are very young!</p>}</p>
+      {age > 18 ? <Age age={age} /> : <p>You are very young!</p>}
     </div>
   );
 }
