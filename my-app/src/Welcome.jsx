78698873c9605b4d169fd5c6b6941417e9611d6e
Robichiri <robertochiriac8@gmail.com>
2024-01-11 14:35:07 +0100

    Conditional Rendering - 2

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -5,13 +5,7 @@ export function Welcome({ name, age }) {
   return (
     <div>
       <p>Welcome, {name}!</p>
-      <p>
-        <Age age={age} />
-        {age > 18 && <Age age={age} />}
-        {age && <Age age={age} />}
-        {age > 18 && age < 65 && <Age age={age} />}
-        {age > 18 && age < 65 && name == "John" && <Age age={age} />}
-      </p>
+      <p>{age > 18 ? <Age age={age} /> : <p>You are very young!</p>}</p>
     </div>
   );
 }
