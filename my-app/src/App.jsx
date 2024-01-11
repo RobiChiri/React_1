acc1e3b36c144231fd1cf823f5a12afa052e67ff
Robichiri <robertochiriac8@gmail.com>
2024-01-11 14:24:15 +0100

    Components- 3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,15 @@
 import { Hello } from "./Hello";
+import { Message } from "./Message";
 
 export function App() {
   return (
     <div>
       <Hello />
+      <Hello />
+      <Message />
     </div>
   );
 }
+
+// il programma funziona regolarmente, è possibile utilizzre più volte lo stesso componente Hello
+// ed è possibile renderizzare Message direttamente in App
