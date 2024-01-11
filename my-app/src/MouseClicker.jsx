49aa6051e48e1c03d9b0af83e928419d28479719
Robichiri <robertochiriac8@gmail.com>
2024-01-11 14:49:11 +0100

    Handling Events - 2

--- my-app/src/MouseClicker.jsx
+++ my-app/src/MouseClicker.jsx
@@ -2,10 +2,19 @@ export function MouseClicker() {
   function handleButtonClick() {
     console.log(event.target.name);
   }
+  function handleImgClick() {
+    console.log(event.target.src);
+  }
 
   return (
     <div>
       <button name="one" onClick={handleButtonClick}>
+        <img
+          src="https://google.com"
+          height={24}
+          width={24}
+          onClick={handleImgClick}
+        ></img>
         ONE
       </button>
     </div>
