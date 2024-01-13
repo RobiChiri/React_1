78eebbce7e97520e2aaab1606fb05dc6211f0e1f
Robichiri <robertochiriac8@gmail.com>
2024-01-13 15:02:53 +0100

    SWR - 1

--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,27 +1,10 @@
-import { useState, useEffect } from "react";
+import useSWR from "swr";
+const fetcher = (url) => fetch(url).then((response) => response.json());
 export function useGithubUser(username) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
+  const { data, error } = useSWR(
+    `https://api.github.com/users/${username}`,
+    fetcher
+  );
 
-  async function fetchGithubUser(username) {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(`https://api.github.com/users/${username}`);
-      const json = await response.json();
-      console.log(json);
-      setData(json);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    fetchGithubUser(username);
-  }, [username]);
-
-  return { data, loading, error };
+  return { data, error };
 }
