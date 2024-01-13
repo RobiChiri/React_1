a355c21aa48ce295e0001ba848ae946f6003ba5e
Robichiri <robertochiriac8@gmail.com>
2024-01-13 15:10:04 +0100

    SWR - 3

--- my-app/src/useGithubUser.jsx
+++ my-app/src/useGithubUser.jsx
@@ -1,7 +1,7 @@
-import { useSWR, mutate } from "swr";
+import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 export function useGithubUser(username) {
-  const { data, error } = useSWR(
+  const { data, error, mutate } = useSWR(
     username ? `https://api.github.com/users/${username}` : null,
     fetcher
   );
