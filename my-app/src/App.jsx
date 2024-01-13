43af119490c547a41f06f1370194369c83d7dcf4
Robichiri <robertochiriac8@gmail.com>
2024-01-13 08:43:37 +0100

    Data Fetching with fetch and useEffect - 1

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,27 +1,9 @@
-import { useState } from "react";
-import { Clock } from "./Clock";
-import { LanguageContext } from "./LanguageContext";
+import { GithubUser } from "./GithubUser";
 
 export function App() {
-  const [language, setLanguage] = useState("en");
-
-  function handleSetLanguage(language) {
-    setLanguage(language);
-  }
   return (
     <div>
-      <LanguageContext.Provider value={language}>
-        <select>
-          <option onChange={() => handleSetLanguage("en")}>EN</option>
-          <option onChange={() => handleSetLanguage("it")}>IT</option>
-        </select>
-
-        {/* <button onClick={() => handleSetLanguage("en")}>EN</button>
-        <button onClick={() => handleSetLanguage("it")}>IT</button> */}
-        {/* utilizzando due button il programma funziona correttamente */}
-
-        <Clock />
-      </LanguageContext.Provider>
+      <GithubUser username="RobiChiri" />
     </div>
   );
 }
---
+++ my-app/src/GithubUser.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+export function GithubUser({ username }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  async function fetchGithubUser(username) {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const json = await response.json();
+      console.log(json);
+      setData(json);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchGithubUser(username);
+  }, [username]);
+
+  return (
+    <div>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error detected...</h1>}
+      {data && (
+        <h1>
+          {data.name} {data.login} {data.avatar_url}
+        </h1>
+      )}
+    </div>
+  );
+}
